56e46678bb1013bd89d0f157771b3068bf4399cc
SwiffterAnim <rodriccosta@gmail.com>
2024-06-18 10:16:13 +0200

    Movement 'kinda' working
    works well if using one of the other from the start. But not if you switch between both after the game has started.

--- Assets/Modules/Movement/Scripts/PlayerMovementController.cs
+++ Assets/Modules/Movement/Scripts/PlayerMovementController.cs
@@ -6,32 +6,59 @@ using UnityEngine.InputSystem;
 public class PlayerMovementController : MonoBehaviour
 {
 
-    [SerializeField] float jumpForce = 350f;
+    [SerializeField] private float jumpForce = 350f;
+
+    private bool gamepadIsConnected = false;
+
+    private Camera mainCamera;
+    private Rigidbody2D rb;
+    private MyInputActions characterActions;
 
     private Vector2 aim;
-    private Rigidbody2D rb;
-    private UnityEngine.InputSystem.Pointer mousePosition;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
+        characterActions = new MyInputActions();
+        characterActions.Enable();
+
+        // Checking if Gamepad is connected. Only does it at Start, so if gamepad is off, start play and THEN turn on gamepad, it will jump towards mouse.
+        // If starts with gamepad, then jumps with mouse, it explodes xD
+        foreach(var device in InputSystem.devices){
+            if(device is Gamepad){
+            gamepadIsConnected = true;
+            break;
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!gamepadIsConnected)
+        {
+            UpdateAimWithMouse();
+        }
     }
 
-/*
-    private void UpdateAim()
+
+    private void UpdateAimWithMouse()
     {
         Vector2 playerPosition = new Vector2 (transform.position.x, transform.position.y);
-        aim = playerPosition - mousePosition.position;
+        Vector2 mousePositionScreen = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        aim = mousePositionScreen - playerPosition;
+        aim.Normalize();
     }
-*/
+
+    private void OnAim(InputValue value)
+    {
+        aim = value.Get<Vector2>();
+    }
+
     private void OnJump(InputValue value)
     {
         if (aim == Vector2.zero)
