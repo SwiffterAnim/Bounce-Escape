77604b4ceaabcccf67a4abfe6e3fd32505f1b93e
SwiffterAnim <rodriccosta@gmail.com>
2024-06-27 09:27:50 +0200

    Created Camera Manager
    Has the logic to shake the camera, but I'm not yet sure where to call it from.

---
+++ Assets/Modules/Camera/Scripts/CameraManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private Camera mainCam;
+    [SerializeField] private float shakeAmmount= 0.02f;
+    [SerializeField] private float shakeFrequency = 0.003f;
+    [SerializeField] private float shakeLength = 0.3f;
+
+
+    private void Awake() {
+        if (mainCam == null)
+        {
+            mainCam = Camera.main;
+        }
+    }
+
+    public void Shake()
+    {
+        InvokeRepeating("DoShake", 0, shakeFrequency);
+        Invoke("StopShake", shakeLength);
+    }
+
+    void DoShake()
+    {
+        if(shakeAmmount > 0)
+        {
+            Vector3 camPos = mainCam.transform.position;
+            
+            float offsetX = Random.value * shakeAmmount * 2 - shakeAmmount;
+            float offsetY = Random.value * shakeAmmount * 2 - shakeAmmount;
+            camPos.x = offsetX;
+            camPos.y = offsetY;
+
+            mainCam.transform.position = camPos;
+        }
+    }
+
+    void StopShake()
+    {
+        CancelInvoke("DoShake");
+        mainCam.transform.localPosition = Vector3.zero;
+    }
+}
